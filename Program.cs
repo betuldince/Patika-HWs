a5fbab80fd16ceabb78ccf60b98762a49ae079bb
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-02 17:08:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main ana fonksiyon default olarak public tir
+            //static bir nesne örneği yaratmadan maine ulaşma
+            //void geri dönmeyecek
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your surname");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba : " + name + " " + lastname);
+        }
+    }
+}
