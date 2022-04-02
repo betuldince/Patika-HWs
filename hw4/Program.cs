dc6c78cbf1990476802dd2cd577b2cc01ed1b458
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-02 18:31:42 +0300

    Update Program.cs

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -1,16 +1,46 @@
-﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace ConsoleProgramming
+
+
+class Solution
 {
-    class Program
+    
+    private static string isWeird(int N){
+        string wr="Not Weird";
+        if(N>100 || N <1){
+            wr="Weird";
+        }
+        if(N%2==1){
+            wr="Weird";
+        }else{
+            if((N>=2 && N<=5) ||N>20){
+               wr="Not Weird";    
+            }else if(N>=6 && N<=20){
+                wr="Weird";
+                
+            }
+        }
+        
+        return wr;
+    }
+    
+    public static void Main(string[] args)
     {
-               static void Main(string[] args)
-        {
-           int time= DateTime.Now.Hour;
-
-           string sonuc= time<=18 ? "good morning" : "good afternoon";
-           Console.WriteLine(sonuc);
-           
-         }
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        string wr= isWeird( N);
+        
+        Console.WriteLine(wr);
     }
 }
