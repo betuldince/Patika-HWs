e8d328e84b7bb6734a945eebb57828b8a21d4df2
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-02 20:44:51 +0300

    Add files via upload

---
+++ hw7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Loops {
+    class Program {
+       
+       static void Main(string[] args){
+       int say=int.Parse(Console.ReadLine());
+
+       int sum= 1;
+       int first=1;
+       while(first<=say){
+           
+           sum=sum*first;
+           first++;
+       }
+       Console.WriteLine(sum);
+
+        string[] araba={"a1", "a2", "a3"};
+        foreach(var a in araba){
+            Console.WriteLine(a);
+
+        }
+       }
+
+
+
+    }
+}
