02d997c1fc233ebd21eadab30b8951e62c32901c
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-02 18:18:32 +0300

    Add files via upload

---
+++ hw4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+               static void Main(string[] args)
+        {
+           int time= DateTime.Now.Hour;
+
+           string sonuc= time<=18 ? "good morning" : "good afternoon";
+           Console.WriteLine(sonuc);
+           
+         }
+    }
+}
