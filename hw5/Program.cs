eab428fbe875e321504583194a4b5620f55ce138
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-02 18:44:25 +0300

    Add files via upload

---
+++ hw5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+               static void Main(string[] args)
+        {
+           int m= DateTime.Now.Month;
+
+           switch(m){
+              case 1:
+              Console.WriteLine("ocak");
+              break;
+
+              case 4:
+              Console.WriteLine("Nisan");
+              break;
+
+
+   
+          
+
+           }
+
+
+           
+         }
+    }
+}
