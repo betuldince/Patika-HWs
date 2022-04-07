efea2e604b9b746cfcccda0e8c255ec921621d59
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-07 22:05:45 +0300

    Add files via upload

---
+++ hw10/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Name
+{
+    class Program{
+
+
+
+        static void Main(string [] args){
+        
+        int a=3;
+        int b=5;
+
+        int sonuc=Multp(a,b);
+       Console.WriteLine(sonuc);
+        
+        func printit= new func();
+        printit.printmet(Convert.ToString(sonuc));
+
+        int c=3;
+        int d=9;
+        int sonuc2=Multp2(ref c, ref d);
+       Console.WriteLine(sonuc2);
+
+        }
+        static int Multp(int bir, int iki){
+            return iki*bir;
+
+        }
+        static int Multp2(ref int bir, ref  int iki){
+            return iki*bir;
+
+        }
+
+
+
+        class func{
+        public void printmet(string var){
+            Console.WriteLine(var);
+        }
+
+        }
+
+
+
+
+    }
+    
+}
