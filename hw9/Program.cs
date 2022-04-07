9bede983be41f90718eaaf5ad1f79c1719fe67b7
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-07 21:35:58 +0300

    Add files via upload

---
+++ hw9/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Name
+{
+    class Program{
+
+
+
+        static void Main(string [] args){
+        
+        int[]sayıdizi={1,5,4,3,2};
+        Array.Sort(sayıdizi);
+
+        // foreach(var say in sayıdizi){
+        //  Console.WriteLine( say);
+
+        // }
+
+        Array.Clear(sayıdizi,2,2);
+
+        // foreach(var say in sayıdizi){
+        //  Console.WriteLine(say);
+        // }
+        
+        Array.Reverse(sayıdizi);
+        foreach(var say in sayıdizi){
+         Console.WriteLine(say);
+        }        
+        
+
+        }
+
+
+
+
+    }
+    
+}
