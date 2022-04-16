10910828e8e2220021162da1ee7b72207dde8267
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-16 20:50:01 +0300

    Add files via upload

---
+++ hw12/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace metotlar{
+
+
+ class program{
+static void Main(string[] args){
+    string sayı="999";
+    bool sonuc=int.TryParse(sayı, out int outsayı);
+    if(sonuc){
+        outsayı++;;
+        Console.WriteLine(outsayı);
+    }
+
+    
+    metot ins =new metot();
+    ins.art(5,6,out int h);
+    Console.WriteLine(h);
+
+    int num=99;
+    int num2=1;
+    ins.printscreen(num,num2);
+}
+
+
+ }   
+class metot{
+
+    public void art(int a,  int b, out int toplam){
+       toplam=a+b;
+
+    }
+    public void printscreen(string var){
+    Console.WriteLine(var);
+    }
+    public void printscreen(int var, int var2){
+    Console.WriteLine(var+var2);
+    }
+
+
+
+}
+
+
+
+}
+
+
+
