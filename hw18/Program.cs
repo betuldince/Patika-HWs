c5037945ae0b4a9c7a44dc0a4158e1c468f9a96b
Betül Dinçer <66443758+betuldince@users.noreply.github.com>
2022-04-17 17:16:01 +0300

    Add files via upload

---
+++ hw18/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+
+namespace metotlar{
+
+
+ class program{
+static void Main(string[] args){
+
+    ArrayList liste = new ArrayList();
+    liste.Add("bet");
+    liste.Add(2);
+    liste.Add(true);
+    liste.Add("harun");
+
+    Console.WriteLine(liste[1]);
+
+    //Add range
+    string[] colors={"blue", "yellow", "green","red"};
+    List<int> numbers= new List<int>(){1,2,3,4,3};
+    liste.AddRange(colors);
+    liste.AddRange(numbers);
+
+    foreach (var item in liste)
+    {
+        Console.WriteLine(item);        
+    }
+
+    // liste.Sort();
+    // liste.BinarySearch(4);
+    // liste.Reverse();
+    // liste.Clear();
+
+    }
+
+ }
+ }   
